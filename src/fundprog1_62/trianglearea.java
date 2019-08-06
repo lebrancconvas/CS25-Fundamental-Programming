42bb4531496f7b35b87c55e2c630b18f28c8050d
lebrancconvas <lebranccconvas@gmail.com>
2019-08-06 23:27:09 +0700

    :tada: Add Find Triangle Area Quiz.

---
+++ src/fundprog1_62/trianglearea.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundprog1_62;
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class trianglearea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Base's Length -> ");
+        double baselength = sc.nextInt();
+        System.out.print("Height -> ");
+        double height = sc.nextInt();
+        double trianglearea = (baselength*height)/2;
+        System.out.println("Triangle Area => " + trianglearea);
+    }
+}
