f7caa001248a55d5ea297691810f999b1069784b
Lebranc Convas <33986491+lebrancconvas@users.noreply.github.com>
2019-09-10 11:58:55 +0700

    Complete GCD.

--- src/fundprog1_62/Lab10Sep2019/gcd.java
+++ src/fundprog1_62/Lab10Sep2019/gcd.java
@@ -1,7 +1,27 @@
-class  
+import java.util.*;
+class gcd
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int p , q;
+		int t;
+		if(x > y) {
+			p = x;
+			q = y;
+		} else {
+			p = y;
+			q = x;
+		}
+
+		for(;q != 0;) {
+			t = q;
+			q = p % q;
+			p = t;
+		}
+
+		System.out.println(p);
 	}
 }
